6ebf5e85279ac55994308a139ef0d9f71f69b964
TrypolskyiS <tripolskys@gmail.com>
2022-03-14 16:22:10 +0200

    Init commit

---
+++ src/main/java/com/example/cryptocurrency_test_task/CryptocurrencyTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptocurrency_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptocurrencyTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cryptocurrency_test_task/CryptocurrencyTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptocurrency_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
