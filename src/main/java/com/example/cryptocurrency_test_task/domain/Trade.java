8486e7183791aa92d0da24f81eddde76564da10f
TrypolskyiS <tripolskys@gmail.com>
2022-03-14 19:47:00 +0200

    Update application.properties to connect to MongoDB. Added TradeRepository.java Updated Trade.java with Id field.

--- src/main/java/com/example/cryptocurrency_test_task/domain/Trade.java
+++ src/main/java/com/example/cryptocurrency_test_task/domain/Trade.java
@@ -4,12 +4,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
+@Document
 public class Trade {
+    @Id
+    private String id;
     private String tid;
     private String type;
     private String amount;
--- src/main/java/com/example/cryptocurrency_test_task/repositories/TradeRepository.java
+++ src/main/java/com/example/cryptocurrency_test_task/repositories/TradeRepository.java
@@ -3,5 +3,5 @@ package com.example.cryptocurrency_test_task.repositories;
 import com.example.cryptocurrency_test_task.domain.Trade;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TradeRepository extends MongoRepository<Trade, Long> {
+public interface TradeRepository extends MongoRepository<Trade, String> {
 }
