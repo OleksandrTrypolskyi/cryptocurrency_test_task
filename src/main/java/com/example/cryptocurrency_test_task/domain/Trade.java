ee66655665611d896d4bc8f30a7d3a584faf0acb
TrypolskyiS <tripolskys@gmail.com>
2022-03-14 17:55:30 +0200

    Add basic domain model: Trade.java

---
+++ src/main/java/com/example/cryptocurrency_test_task/domain/Trade.java
@@ -0,0 +1,19 @@
+package com.example.cryptocurrency_test_task.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Trade {
+    private String tid;
+    private String type;
+    private String amount;
+    private String price;
+    private String date;
+    private String currency;
+}
